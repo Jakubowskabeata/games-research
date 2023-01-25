cf222eb772aa63c4ad57f923d57fe6da5fae6a17
Beata <beata.jaqubowska@gmail.com>
2023-01-25 18:51:13 +0100

    connect to the api

--- src/Axios.js
+++ src/Axios.js
@@ -1,35 +1,148 @@
 import React, { Component } from "react";
 import { getApiRawg } from "./api.js";
 
+import PropTypes from 'prop-types';
+
 export default class Axios extends Component {
 	constructor() {
 		super();
+const propTypes = {
+	count: PropTypes.number,
+	next: PropTypes.string,
+	previous: PropTypes.any,
+	results: PropTypes.arrayOf(
+		PropTypes.shape({
+			slug: PropTypes.string,
+			name: PropTypes.string,
+			playtime: PropTypes.number,
+			platforms: PropTypes.arrayOf(
+				PropTypes.shape({
+					platform: PropTypes.shape({
+						id: PropTypes.number,
+						name: PropTypes.string,
+						slug: PropTypes.string,
+					}),
+				})
+			),
+			stores: PropTypes.arrayOf(
+				PropTypes.shape({
+					store: PropTypes.shape({
+						id: PropTypes.number,
+						name: PropTypes.string,
+						slug: PropTypes.string,
+					}),
+				})
+			),
+			released: PropTypes.string,
+			tba: PropTypes.bool,
+			background_image: PropTypes.string,
+			rating: PropTypes.number,
+			rating_top: PropTypes.number,
+			ratings: PropTypes.arrayOf(
+				PropTypes.shape({
+					id: PropTypes.number,
+					title: PropTypes.string,
+					count: PropTypes.number,
+					percent: PropTypes.number,
+				})
+			),
+			ratings_count: PropTypes.number,
+			reviews_text_count: PropTypes.number,
+			added: PropTypes.number,
+			added_by_status: PropTypes.shape({
+				yet: PropTypes.number,
+				owned: PropTypes.number,
+				beaten: PropTypes.number,
+				toplay: PropTypes.number,
+				dropped: PropTypes.number,
+				playing: PropTypes.number,
+			}),
+			metacritic: PropTypes.number,
+			suggestions_count: PropTypes.number,
+			updated: PropTypes.string,
+			id: PropTypes.number,
+			score: PropTypes.any,
+			clip: PropTypes.any,
+			tags: PropTypes.arrayOf(
+				PropTypes.shape({
+					id: PropTypes.number,
+					name: PropTypes.string,
+					slug: PropTypes.string,
+					language: PropTypes.string,
+					games_count: PropTypes.number,
+					image_background: PropTypes.string,
+				})
+			),
+			esrb_rating: PropTypes.shape({
+				id: PropTypes.number,
+				name: PropTypes.string,
+				slug: PropTypes.string,
+				name_en: PropTypes.string,
+				name_ru: PropTypes.string,
+			}),
+			user_game: PropTypes.any,
+			reviews_count: PropTypes.number,
+			saturated_color: PropTypes.string,
+			dominant_color: PropTypes.string,
+			short_screenshots: PropTypes.arrayOf(
+				PropTypes.shape({
+					id: -PropTypes.number,
+					image: PropTypes.string,
+				})
+			),
+			parent_platforms: PropTypes.arrayOf(
+				PropTypes.shape({
+					platform: PropTypes.shape({
+						id: PropTypes.number,
+						name: PropTypes.string,
+						slug: PropTypes.string,
+					}),
+				})
+			),
+			genres: PropTypes.arrayOf(
+				PropTypes.shape({
+					id: PropTypes.number,
+					name: PropTypes.string,
+					slug: PropTypes.string,
+				})
+			),
+		})
+	),
+	user_platforms: PropTypes.bool,
+};
 
-		this.state = {
-			apiRawg: [],
-		};
+		this.state = 
+		 this.propTypes
+		;
 	}
 
-	componentDdMount() {
+	componentDidMount() {
 		getApiRawg()
 			.then((response) => {
-				console.log(response);
-				this.setState({
-					apiRawg: response.data,
-				});
+				//console.log(response);
+				this.setState(
+					response.data,
+				);
 			})
 			.catch((err) => console.log(err.message));
 	}
 
 	render() {
-		const { apiRawg } = this.state;
+		if (this.state) {
+			const apiGames  = this.state;
 
-		console.log(apiRawg);
+			console.log(apiGames);
 
-		const apiRawgList = apiRawg.map((item, index) => {
-			return <div key={`${item.title}${index}`}>{item.title}</div>;
-		});
+			const apiRawgList = apiGames.results.map((item) => {
+				return <div key={item.id}>{item.name}</div>;
+			});
 
-		return <div>{apiRawgList}</div>;
+			return (
+				<div>
+					<p>Lista gier:</p>
+					{apiRawgList}
+				</div>
+			);
+		}
 	}
 }
--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
 const API_RAWG_URL =
-	"https://api.rawg.io/api/platforms?key=342f8b20c974442f9c072609d2653d5e";
+	"https://api.rawg.io/api/games?key=342f8b20c974442f9c072609d2653d5e&dates=2019-09-01,2019-09-30&platforms=18,1,7";
+	
 
 export function getApiRawg() {
 	return axios.get(API_RAWG_URL);
